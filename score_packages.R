69842dadae44da4b214d8c11242be3dfea47558b
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2020-06-29 09:08:02 +0200

    read installed pkgs from file.

--- score_packages.R
+++ score_packages.R
@@ -1,13 +1,18 @@
+#Rlibdir <- paste0('./.Rlibs/', R.version$major, substr(R.version$minor, start = 1, stop = 1))
+#.libPaths(c(.libPaths(), Rlibdir))
+
 library(dplyr)
 library(riskmetric)
 
 # installed packages
-ip <- read.csv('data/R361_installed_packages.csv', stringsAsFactors = FALSE)
+#ip <- read.csv('data/R361_installed_packages.csv', stringsAsFactors = FALSE)
+ip <- as.data.frame(installed.packages(), stringsAsFactors = FALSE)
 
 # score packages
 sp <- pkg_ref(ip$Package) %>%
   as_tibble() %>%
   pkg_assess() %>%
-  pkg_score()
+  pkg_score() %>% 
+  mutate(risk = summarize_scores(.))
 
-write.csv(sp, file = 'data/R361_scored_packages.csv')
+write.csv(sp, file = 'data/R363_scored_packages.csv')
